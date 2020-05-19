2d2d0ca6919667136882dddb1e2e8cbd42c56108
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-05-19 18:13:10 +0200

    created tests for Customer class

--- RefactoringProject/src/test/java/CustomerTest.java
+++ RefactoringProject/src/test/java/CustomerTest.java
@@ -3,16 +3,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
-
-    @Test
-    void addRental() {
-    }
+    Customer customerTest = new Customer("nameTest");
 
     @Test
     void getName() {
+        assertEquals("nameTest", customerTest.getName());
     }
 
     @Test
     void statement() {
+        String result = "Rental Record for " + "nameTest" + "\n";
+        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        result += "Amount owed is " + "0.0" + "\n";
+        result += "You earned " + "0" + " frequent renter points";
+        assertEquals(result, customerTest.statement());
+    }
+
+    Movie movieTest = new Movie("movieTest", 10);
+    Rental rentalTest = new Rental(movieTest, 15);
+    
+    @Test
+    void addRental() {
+        customerTest.addRental(rentalTest);
+
+        String statement = "Rental Record for nameTest" + "\n";
+        statement += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        statement += "\t" + "movieTest" + "\t" + "\t" + "15" + "\t" + "0.0" + "\n";
+        statement += "Amount owed is " + "0.0" + "\n";
+        statement += "You earned " + "1" + " frequent renter points";
+
+        assertEquals(statement, customerTest.statement());
     }
 }
