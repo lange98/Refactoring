2233a9480220098b8a11035a559dc00ea8982743
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-05-19 17:53:53 +0200

    created tests for rental class

--- RefactoringProject/src/test/java/RentalTest.java
+++ RefactoringProject/src/test/java/RentalTest.java
@@ -3,12 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RentalTest {
+    Movie movieTest = new Movie("testTitle", 10);
+    Rental rentalTest = new Rental(movieTest, 10);
 
     @Test
     void getDaysRented() {
+        assertEquals(10, rentalTest.getDaysRented());
     }
 
     @Test
     void getMovie() {
+        assertEquals(movieTest, rentalTest.getMovie());
     }
 }
