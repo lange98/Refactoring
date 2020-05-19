bcba3a48262f731c054e7b8dbb740fe373ed2122
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-05-19 17:45:45 +0200

    created tests for Movie Class

--- RefactoringProject/src/test/java/MovieTest.java
+++ RefactoringProject/src/test/java/MovieTest.java
@@ -4,15 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
 
+    Movie testMovie = new Movie("testTitle", 10);
     @Test
     void getPriceCode() {
+        assertEquals(10, testMovie.getPriceCode());
     }
 
     @Test
     void setPriceCode() {
+        testMovie.setPriceCode(15);
+        assertEquals(15, testMovie.getPriceCode());
     }
 
     @Test
     void getTitle() {
+        assertEquals("testTitle", testMovie.getTitle());
     }
 }
