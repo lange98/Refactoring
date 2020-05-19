ea7763bd281d3453fb78ef78e247ff3040558c94
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-05-19 19:22:04 +0200

    Replacing the Conditional Logic on Price Code with Polymorphism

--- RefactoringProject/src/main/java/Movie.java
+++ RefactoringProject/src/main/java/Movie.java
@@ -4,17 +4,51 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
+
+    public Movie(String newtitle, int newpriceCode)
+    {
         title = newtitle;
         priceCode = newpriceCode;
     }
+
     public int getPriceCode() {
         return priceCode;
     }
+
     public void setPriceCode(int arg) {
         priceCode = arg;
     }
+
     public String getTitle (){
         return title;
-    };
-}
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (priceCode) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+    public int getFrequentRenterPoints(int daysRented) {
+        int result = 0;
+        // add frequent renter points
+        result++;
+        // add bonus for a two day new release rental
+        if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+            result++;
+        return result;
+    }
+};
--- RefactoringProject/src/main/java/Rental.java
+++ RefactoringProject/src/main/java/Rental.java
@@ -1,10 +1,13 @@
 class Rental {
     private Movie movie;
     private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
+
+    public Rental(Movie newmovie, int newdaysRented)
+    {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
@@ -13,30 +16,12 @@ class Rental {
         return movie;
     }
 
-    public double calcCost() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    public double calcCost()
+    {
+        return movie.getCharge(daysRented);
     }
-    public int calcFrequentRenterPoints() {
-        int result = 0;
-        result++;
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            result++;
-        return result;
+    public int calcFrequentRenterPoints()
+    {
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
